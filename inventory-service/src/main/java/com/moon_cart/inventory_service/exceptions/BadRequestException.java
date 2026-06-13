9d9c78718950067916ca756a53eebc4c74b57665
praveen kavali <kavali@praveen.localdomain>
2026-06-13 13:56:19 +0000

    Removed inventory-service

--- inventory-service/src/main/java/com/moon_cart/inventory_service/exceptions/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.moon_cart.inventory_service.exceptions;
-
-public class BadRequestException extends InventoryExceptions{
-    public BadRequestException(String message, String errorCode, int statusCode) {
-        super(message, errorCode, statusCode);
-    }
-}
--- inventory-service/src/main/java/com/moon_cart/inventory_service/exceptions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.moon_cart.inventory_service.exceptions;
-
-public class NotFoundException extends InventoryExceptions{
-    public NotFoundException(String message, String errorCode, int statusCode) {
-        super(message, errorCode, statusCode);
-    }
-}
