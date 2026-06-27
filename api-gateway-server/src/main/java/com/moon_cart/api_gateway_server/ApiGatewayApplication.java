038e8ec98399842dd51283bf0100f06e33177070
praveen kavali <kavali@praveen.localdomain>
2026-06-27 06:53:20 +0000

    implemented discovery-server, api-gateway-server and readme.md

---
+++ api-gateway-server/src/main/java/com/moon_cart/api_gateway_server/ApiGatewayApplication.java
@@ -0,0 +1,4 @@
+package com.moon_cart.api_gateway_server;
+
+public class ApiGatewayApplication {
+}
---
+++ api-gateway-server/src/main/java/com/moon_cart/api_gateway_server/route/Route.java
@@ -0,0 +1,14 @@
+package com.moon_cart.api_gateway_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouterFunction inventoryRoute() {
+        
+    }
+}
